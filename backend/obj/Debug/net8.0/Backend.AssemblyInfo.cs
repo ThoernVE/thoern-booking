8d10de5141234919be087208b11e4dc2efbc099b
Thoernve <viktor.e.thorn@hotmail.com>
2025-09-11 09:29:29 +0200

    Setup dockerhandling aswell as workflow to automatically push to host with selfhosted runner

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d691e37a6f515115544df6395615d45ff70063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a4e619ddea2251bceaa9729fe94040ea1d5627")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
