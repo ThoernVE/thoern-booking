0021707cfcc10dd9228f7021cf8b20b8e7ad82b2
Thoernve <viktor.e.thorn@hotmail.com>
2025-09-11 17:21:49 +0200

    added files to gitignore

---
+++ src/Landingpage/Landingpage.tsx
@@ -0,0 +1,18 @@
+import { Row, Col } from "react-bootstrap";
+
+LandingPage.route = {
+  path: '/',
+  menuLabel: 'Home',
+  index: 1,
+};
+
+export default function LandingPage() {
+    return <>
+        <Row>
+          <Col>
+            <h1 className="text-center">Page is under construction</h1>
+            <h3>Feel free to check out <a href="https://thoernve.dev">the main page</a> while waiting!</h3>
+          </Col>
+        </Row>
+      </>;
+    }
