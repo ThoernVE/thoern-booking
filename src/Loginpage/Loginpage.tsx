49a70b82a680fe9def2289a5af3ff675c0a9a1fe
Thoernve <viktor.e.thorn@hotmail.com>
2025-09-17 12:59:33 +0200

    Redid textfields on loginpage to look cleaner

--- src/Loginpage/Loginpage.tsx
+++ src/Loginpage/Loginpage.tsx
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap";
 import { Form } from "react-bootstrap";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 
 
 Loginpage.route = {
@@ -12,24 +12,27 @@ Loginpage.route = {
 export default function Loginpage() {
     return (
         <>
-            <Row>
-                <Col className="section-full d-flex flex-column justify-content-center align-items-center p-5 border bg-surface-accent shadow-sm">
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Button variant="accent" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                    <a href="/register" className="mt-5">Or sign up for a new account</a>
-                </Col >
-            </Row >
+            <Container>
+                <Row>
+                    <Col className="section-full d-flex flex-column justify-content-center align-items-center p-5 border bg-surface-accent shadow-sm">
+                        <h1 className="mb-5 fw-bold">Log In</h1>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="accent" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                        <p className="mt-5">Are you new here? <a href="/register">Sign up</a></p>
+                    </Col >
+                </Row >
+            </Container>
         </>
     );
 }
