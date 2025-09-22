1e78a8964411e7fe8ab64afb3757065ce1bbca44
Thoernve <viktor.e.thorn@hotmail.com>
2025-09-22 18:59:48 +0200

    Added dashboard for users who have logged in

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,28 +1,24 @@
 import { Row, Col, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthProvider';
 
 Dashboard.route = {
     path: '/dashboard',
     menuLabel: 'Dashboard',
-    index: 3
+    index: 3,
+    protected: true,
+    roles: ["freelancer", "user"],
 };
 
 
 
 export default function Dashboard() {
+    const { logout } = useAuth();
     const navigator = useNavigate();
 
-    async function logout() {
+    async function handleLogout() {
         try {
-            const response = await fetch("/api/login", {
-                method: "DELETE",
-                headers: { "Content-Type": "application/json" },
-            });
-
-            if (!response.ok) {
-                console.error(response.status)
-                throw new Error("Logout failed");
-            }
+            await logout();
             console.log("User succesfully logged out");
             navigator("/");
         }
@@ -36,7 +32,7 @@ export default function Dashboard() {
             <Col>
                 <h2 className="text-primary">About me</h2>
                 <p>Hello! I am Viktor. Read more at <a href="https://thoernve.dev">my webpage</a></p>
-                <Button onClick={logout}>Logout</Button>
+                <Button onClick={handleLogout}>Logout</Button>
             </Col>
         </Row>
     </>;
